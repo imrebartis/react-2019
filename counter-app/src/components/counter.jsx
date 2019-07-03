b47861a1a0f316a22548b41c6fceb0bb1c4c6448
Imre Bartis <imre.bartis@siili.com>
2019-07-03 14:16:11 +0300

    update the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,7 +6,7 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    console.log("increment clicked", this);
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() {
