f1333d13f7c1538d44a63127472a751b447d5771
Imre Bartis <imre.bartis@siili.com>
2019-07-03 12:59:35 +0300

    use React.Fragment to avoid unnecessary divs

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Heyou</h1>;
+    return (
+      <React.Fragment>
+        <h1>Heyou</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
