27b976183aabfb21ffd9bafd2fb145c3697ffb76
Imre Bartis <imre.bartis@siili.com>
2019-07-03 14:29:39 +0300

    pass event argument

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,7 +5,8 @@ class Counter extends Component {
     count: 0
   };
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   }
 
@@ -16,7 +17,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
