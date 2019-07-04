c57c4d0271c7dbee105e37ce2e91d8def08fae71
Imre Bartis <imre.bartis@siili.com>
2019-07-04 14:26:09 +0300

    wire up updating the state

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,24 +4,28 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 3},
-      { id: 2, value: 0},
-      { id: 3, value: 0},
-      { id: 4, value: 0}
+      { id: 1, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
   };
 
-  handleDelete = () => {
-    console.log('event handler called');
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
-        { this.state.counters.map(counter =>
-          <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>)}
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
       </div>
     );
   }
