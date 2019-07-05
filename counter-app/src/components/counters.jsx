220b64e521339d2290f1354337cb5625386a1e7f
Imre Bartis <imre.bartis@siili.com>
2019-07-05 14:44:22 +0300

    add decrement button

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -3,20 +3,21 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
-    const {onReset, counters, onDelete, onIncrement} = this.props;
+    const {onReset, counters, onDelete, onIncrement, onDecrement} = this.props;
 
     return (
-      <div>
+      <React.Fragment>
       <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
-      </div>
+      </React.Fragment>
     );
   }
 }
