4a8ef2f0e1200e5aafc08a736e52b9c2568c9c39
Imre Bartis <imre.bartis@siili.com>
2019-07-05 13:06:41 +0300

    sync multiple components

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -35,7 +35,7 @@ class Counters extends Component {
   render() {
     return (
       <div>
-      <button onClick={this.handleReset} className="btn btn-danger btn-sm m-2">Reset</button>
+      <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
