b9bccd461d32253ebe4b2428697b0729259b39e8
Imre Bartis <imre.bartis@siili.com>
2019-07-08 13:07:45 +0300

    set up genre selection handling

--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-const ListGroup = (props) => {
-  const { items, valueProperty, textProperty } = props;
+const ListGroup = props => {
+  const {
+    items,
+    valueProperty,
+    textProperty,
+    selectedItem,
+    onItemSelect
+  } = props;
   return (
     <ul className="list-group">
-      { items.map(item => <li key={item[valueProperty]} className="list-group-item">{item[textProperty]}</li>)}
+      {items.map(item => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item[textProperty]}
+        </li>
+      ))}
     </ul>
   );
 };
