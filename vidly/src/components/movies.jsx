d828c430b32111d65baee9015ff681a6e9b92e3c
Imre Bartis <imre.bartis@siili.com>
2019-07-08 09:37:30 +0300

    add like component

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -10,6 +11,14 @@ class Movies extends Component {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  }
 
   render() {
     const { length: count } = this.state.movies;
@@ -25,6 +34,7 @@ class Movies extends Component {
                 <th>Stock</th>
                 <th>Rate</th>
                 <th />
+                <th />
             </tr>
             </thead>
             <tbody>
@@ -34,6 +44,9 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onLikeToggle={() => this.handleLike(movie)}/>
+                </td>
                 <td>
                     <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
                 </td>
