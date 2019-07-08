86978fa6f77923689b3810544e4ae81fd532f77b
Imre Bartis <imre.bartis@siili.com>
2019-07-08 11:20:09 +0300

    handle page changes for pagination

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -6,6 +6,7 @@ import { getMovies } from "../services/fakeMovieService";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
     pageSize: 10
   };
 
@@ -23,11 +24,13 @@ class Movies extends Component {
   };
 
   handlePageChange = page => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
     return (
       <React.Fragment>
@@ -70,7 +73,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
