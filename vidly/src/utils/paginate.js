ecfb28598b163d70d37a75fdfad952ff2a722189
Imre Bartis <imre.bartis@siili.com>
2019-07-08 14:08:19 +0300

    extract moviesTable; simplify paginate.js

--- vidly/src/utils/paginate.js
+++ vidly/src/utils/paginate.js
@@ -1,10 +1,12 @@
-import _ from "lodash";
+// import _ from "lodash";
 
 export function paginate(items, pageNumber, pageSize) {
   const startIndex = (pageNumber - 1) * pageSize;
 
-  return _(items) // lodash wrapper object that enables methods chaining
-    .slice(startIndex)
-    .take(pageSize) // pageSize is the nr of items from the lodash wrapper obj that we want to use
-    .value(); // turn lodash wrapper object into an array
+  // return _(items) // lodash wrapper object that enables methods chaining
+  //   .slice(startIndex)
+  //   .take(pageSize) // pageSize is the nr of items from the lodash wrapper obj that we want to use
+  //   .value(); // turn lodash wrapper object into an array
+
+  return items.slice(startIndex, startIndex + pageSize);
 }
