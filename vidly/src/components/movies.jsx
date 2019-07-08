70d7cf57e2d4d62c9dc3e6dc9b0137696654c3de
Imre Bartis <imre.bartis@siili.com>
2019-07-08 10:24:52 +0300

    set up component interface for pagination

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   };
 
   handleDelete = movie => {
@@ -12,48 +14,65 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
-  handleLike = (movie) => {
+  handleLike = movie => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
-  }
+  };
+
+  handlePageChange = page => {
+    console.log(page);
+  };
 
   render() {
     const { length: count } = this.state.movies;
-    if (count === 0) return <p>There are no movies in the database.</p>
+    if (count === 0) return <p>There are no movies in the database.</p>;
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database.</p>
         <table className="table">
-            <thead>
+          <thead>
             <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             {this.state.movies.map(movie => (
-                <tr key={movie._id}>
+              <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <Like liked={movie.liked} onLikeToggle={() => this.handleLike(movie)}/>
+                  <Like
+                    liked={movie.liked}
+                    onLikeToggle={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
-                    <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
                 </td>
-                </tr>
+              </tr>
             ))}
-            </tbody>
+          </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
