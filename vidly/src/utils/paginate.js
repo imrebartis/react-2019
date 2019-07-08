335cbb146b970ec90c887e9002b37166908aeb52
Imre Bartis <imre.bartis@siili.com>
2019-07-08 12:05:57 +0300

    set up data pagination

---
+++ vidly/src/utils/paginate.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+export function paginate(items, pageNumber, pageSize) {
+  const startIndex = (pageNumber - 1) * pageSize;
+
+  return _(items) // lodash wrapper object that enables methods chaining
+    .slice(startIndex)
+    .take(pageSize) // pageSize is the nr of items from the lodash wrapper obj that we want to use
+    .value(); // turn lodash wrapper object into an array
+}
