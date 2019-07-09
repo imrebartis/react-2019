bdea17e0d21aa6d42e64eb944007c849270769c7
Imre Bartis <imre.bartis@siili.com>
2019-07-09 11:09:48 +0300

    make the moviesTable component easier to re-use

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -10,14 +10,14 @@ const Pagination = props => {
   const pages = _.range(1, pagesCount + 1);
 
   return (
-    <nav>
+    <nav aria-label="Page navigation">
       <ul className="pagination">
         {pages.map(page => (
           <li
             key={page}
             className={page === currentPage ? "page-item active" : "page-item"}
           >
-            <a className="page-link" onClick={() => onPageChange(page)}>
+            <a href="/#" className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
