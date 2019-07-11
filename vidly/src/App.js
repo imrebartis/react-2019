e6057cbc69d3d7680b69ea059831652ee5c587e2
Imre Bartis <imre.bartis@siili.com>
2019-07-11 09:33:30 +0300

    add routes

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Movies from "./components/movies";
+import { Route, Redirect, Switch } from 'react-router-dom';
 import "./App.css";
+import Movies from "./components/movies";
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
