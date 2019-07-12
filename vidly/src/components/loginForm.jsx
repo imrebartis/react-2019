a3ef83fe37017c08765538960b8d1dd052eb3e0f
Imre Bartis <imre.bartis@siili.com>
2019-07-12 13:03:31 +0300

    extract reusable form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,65 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
+import Form from "./common/form";
 import Input from "./common/input";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
   schema = {
-    username: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password")
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return "";
-
-    const errors = {};
-    // for (let item of error.details)
-    //   errors[item.path[0]] = item.message;
-    error.details.map(item => errors[item.path[0]] = item.message);
-    return errors;
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const errors = this.validate();
-
-    this.setState({ errors });
-    if (errors) return;
-
+  doSubmit = () => {
     // call the server
     console.log("submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    // use computed property (i.e. [name]) => whatever name is at runtime
-    // it will be set to value
-   const obj = { [name]: value };
-   const schema = { [name]: this.schema[name] };
-   const { error } = Joi.validate(obj, schema);
-   return error ? error.details[0].message : null;
-  }
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = {...this.state.errors};
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
 
     return (
       <div>
@@ -67,20 +32,22 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
-            value={account.username}
+            value={data.username}
             label="Username"
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name="password"
-            value={account.password}
+            value={data.password}
             label="Password"
             onChange={this.handleChange}
             error={errors.password}
           />
 
-          <button disabled={this.validate()} className="btn btn-primary">Log in</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Log in
+          </button>
         </form>
       </div>
     );
