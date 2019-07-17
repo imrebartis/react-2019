915a2783697ba2ff6e2ba14057132f5ca61abc81
Imre Bartis <imre.bartis@siili.com>
2019-07-17 11:47:44 +0300

    refactor code

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -36,21 +36,26 @@ class MovieForm extends Form {
       .label("Daily rental rate")
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
-    }
-    catch (ex) {
+    } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel = movie => {
