20af45317680d82e3221d79a6ad3da4408dff7e8
Imre Bartis <imre.bartis@siili.com>
2019-07-17 12:20:05 +0300

    refactor code

--- vidly/src/services/movieService.js
+++ vidly/src/services/movieService.js
@@ -3,12 +3,16 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = `${apiUrl}/movies`;
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(`${apiEndpoint}/${movieId}`);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
@@ -17,12 +21,12 @@ export function saveMovie(movie) {
     // which is part of the state (see doSubmit)
     const body = {...movie};
     delete body._id;
-    return http.put(`${apiEndpoint}/${movie._id}`, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(`${apiEndpoint}`, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(`${apiEndpoint}/${movieId}`)
+  return http.delete(movieUrl(movieId))
 }
